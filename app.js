289c26007a74942a27e050e3dbbc2ae705939c56
Kim Gerst <kimgerst@gmail.com>
2015-05-16 12:39:35 -0500

    successfully streaming data via socket.io

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var serialport = require('serialport');
+var SerialPort = serialport.SerialPort
+var sp = new SerialPort("/dev/cu.usbmodem1421", {
+  baudrate: 9600,
+  parser: serialport.parsers.raw
+});
 
 app.get('/', function(req, res){
   res.sendfile('index.html');
@@ -8,8 +14,27 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  io.emit('chat message', 'hi');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+sp.open(function (error) {
+  if ( error ) {
+    console.log('failed to open: '+error);
+  } else {
+    console.log('open');
+    io.emit('chat message', 'serial port connection is open');
+    sp.on('data', function(data) {
+      io.emit('chat message', data);
+      console.log(data.toString());
+    });
+  }
 });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
+
+
