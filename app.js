8b72d999ac3578c3e656a44d5f01b372e86efa0a
Kim Gerst <kimgerst@gmail.com>
2015-05-16 12:15:18 -0500

    socket.io helloworld

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.get('/', function(req, res) {
-  res.send('<h1>Hello world</h1>');
+app.get('/', function(req, res){
+  res.sendfile('index.html');
 });
 
-http.listen(3000, function() {
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+http.listen(3000, function(){
   console.log('listening on *:3000');
 });
