31076af9e8a6581533335f174b0dc89b26ff308b
Kim Gerst <kimgerst@gmail.com>
2015-05-16 13:49:21 -0500

    end to end with fake date from arduino

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var serialport = require('serialport');
 var SerialPort = serialport.SerialPort
 var sp = new SerialPort("/dev/cu.usbmodem1421", {
   baudrate: 9600,
-  parser: serialport.parsers.raw
+  // baudrate: 57600,
+  parser: serialport.parsers.readline("\n")
 });
 
 app.get('/', function(req, res){
-  res.sendfile('index.html');
+  res.sendfile('public/index.html');
 });
+
+app.use(express.static('public'));
 
 io.on('connection', function(socket){
   console.log('a user connected');
@@ -36,5 +40,3 @@ sp.open(function (error) {
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
-
-
---
+++ public/js/main.js
@@ -0,0 +1,138 @@
+/***
+ *       __  __________  _____________ ______________  _  __
+ *      /  |/  / __/ _ \/  _/_  __/ _ /_  __/  _/ __ \/ |/ /
+ *     / /|_/ / _// // // /  / / / __ |/ / _/ // /_/ /    / 
+ *    /_/  /_/___/____/___/ /_/ /_/ |_/_/ /___/\____/_/|_/  
+ *                                                          
+ */
+//set variables
+var levelOfMeditation = 55;
+var meditationColor = "#FF5131";
+
+var socket = io();
+socket.on('chat message', function(msg) {
+    levelOfMeditation = msg.split(',')[2];
+    console.log('Meditation: ' + levelOfMeditation);    
+});
+
+
+//set up SVG
+var meditationVis = d3.select("#meditation").append("svg");
+var meditationCircle = d3.select("#meditation svg").append("circle");
+
+//svg and circle set-up
+meditationVis.attr("width", "600").attr("height", "600"); // Added height and width so svgs are visible
+
+meditationCircle.attr('class','meditationCircle')
+      .attr('cy','250')
+      .attr('cx','250')
+      .attr('r', '20')
+      .attr('fill-opacity', '0');
+
+function setMeditationColor() {
+  if (levelOfMeditation <= 51) {
+    meditationColor = "#00FFFF";
+    meditationOpacity = "0.2";
+  } else if (levelOfMeditation <= 102) {
+    meditationColor = "#33FFFF";
+    meditationOpacity = "0.4";
+  } else if (levelOfMeditation <= 153) {
+    meditationColor = "#66FFFF";
+    meditationOpacity = "0.6";
+  } else if (levelOfMeditation <= 204) {
+    meditationColor = "#99FFFF";
+    meditationOpacity = "0.8";
+  } else if (levelOfMeditation <= 255) {
+    meditationColor = "#CCFFFF";
+    meditationOpacity = "1";
+  }
+}
+
+  //update circle color and radius
+  setInterval(function(){ 
+    setMeditationColor();
+        meditationCircle
+        .transition()
+        .duration(1000)
+        .attr('fill', meditationColor)
+        .attr('r', levelOfMeditation)
+        .attr('fill-opacity', meditationOpacity);
+  }, 1000);
+
+// setInterval(function(){ 
+//     levelOfMeditation = Math.random() * (0 - 255) + 255;
+// }, 10000);
+
+
+
+/***
+ *       ___ _________________  ________________  _  __
+ *      / _ /_  __/_  __/ __/ |/ /_  __/  _/ __ \/ |/ /
+ *     / __ |/ /   / / / _//    / / / _/ // /_/ /    / 
+ *    /_/ |_/_/   /_/ /___/_/|_/ /_/ /___/\____/_/|_/  
+ *                                                     
+ */
+//set variables
+var levelOfAttention = 55;
+
+socket.on('chat message', function(msg) {
+    levelOfAttention = msg.split(',')[1];
+    console.log('Attention: ' + levelOfAttention);    
+});
+
+var attentionColor = "#FF5131";
+var attentionCircle = d3.select("#meditation svg").append("circle");
+
+attentionCircle.attr('class','attentionCircle')
+      .attr('cy','250')
+      .attr('cx','250')
+      .attr('r', '20')
+      .attr('fill-opacity', '0');
+
+function setAttentionColor() {
+  if (levelOfAttention <= 51) {
+    attentionColor = "#FFCCCC";
+    attentionOpacity = "0.2";
+  } else if (levelOfAttention <= 102) {
+    attentionColor = "#FF9999";
+    attentionOpacity = "0.4";
+  } else if (levelOfAttention <= 153) {
+    attentionColor = "#FF6666";
+    attentionOpacity = "0.6";
+  } else if (levelOfAttention <= 204) {
+    attentionColor = "#FF3333";
+    attentionOpacity = "0.8";
+  } else if (levelOfAttention <= 255) {
+    attentionColor = "#FF0000";
+    attentionOpacity = "1";
+  }
+}
+
+  //update circle color and radius
+  setInterval(function(){ 
+    setAttentionColor();
+        attentionCircle
+        .transition()
+        .duration(1000)
+        .attr('fill', attentionColor)
+        .attr('r', levelOfAttention)
+        .attr('fill-opacity', attentionOpacity);
+  }, 1000);
+
+// setInterval(function(){ 
+//     levelOfAttention = Math.random() * (0 - 255) + 255;
+// }, 1000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
